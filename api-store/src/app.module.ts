3af2c81e588ef1b63eeeb16bb25a8be5097dd593
Bartolomé Des <69482682+bartodes@users.noreply.github.com>
2023-04-09 21:07:25 -0300

    Update app.module.ts

--- api-store/src/app.module.ts
+++ api-store/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StoreModule } from './store/store.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://admin:password1234@10.97.243.148:27017/?authMechanism=DEFAULT'),
+    MongooseModule.forRoot('mongodb://root:root_password@localhost:27017/?authMechanism=DEFAULT'),
     StoreModule,
   ],
   controllers: [AppController],
