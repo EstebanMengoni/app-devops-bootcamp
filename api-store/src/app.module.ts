c57b189c57942f79e53d5a7272ffabd7a11efbdc
Bartolomé Des <69482682+bartodes@users.noreply.github.com>
2023-04-13 02:32:28 -0300

    feat: mongo container service name

--- api-store/src/app.module.ts
+++ api-store/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StoreModule } from './store/store.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:root_password@localhost:27017/?authMechanism=DEFAULT'),
+    MongooseModule.forRoot('mongodb://root:root_password@mongodb-service:27017/?authMechanism=DEFAULT'),
     StoreModule,
   ],
   controllers: [AppController],
